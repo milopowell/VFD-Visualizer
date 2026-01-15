8a8ddbb7792659570fbcb66ade19790450fa41ff
MiloPowell <milo.powell@icloud.com>
2026-01-15 18:12:56 +0600

    Initial Commit

---
+++ VFD Visualizer/VFD_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VFD_VisualizerApp.swift
+//  VFD Visualizer
+//
+//  Created by Milo Powell on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct VFD_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
